7169f1d9720810f5b9bebcf2a4135bedd652c286
Thais Cardoso <thaistauane.cardoso@gmail.com>
2022-05-22 19:03:04 -0300

    lint: corrigindo erro de lint

--- routes/talker.js
+++ routes/talker.js
@@ -17,17 +17,17 @@ routes.get('/search', (req, res) => {
         return res.status(200).json(talker);
     }
 
-    const talkerperson = talker.filter((el) =>
-     el.name.toLowerCase().includes(name.toLocaleLowerCase()));
+    const talkerperson = talker.filter((user) =>
+     user.name.toLowerCase().includes(name.toLocaleLowerCase()));
 
     res.status(200).json(talkerperson);
 });
 
 routes.get('/:id', (req, res) => {
     const { id } = req.params;
-    const talkerId = talker.find((el) => el.id === Number(id));
+    const talkerId = talker.find((user) => user.id === Number(id));
 
-    if(!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    if (!talkerId) return res.status(404).json({ message: 'User not Found' });
 
     res.status(200).json(talkerId);
 });
