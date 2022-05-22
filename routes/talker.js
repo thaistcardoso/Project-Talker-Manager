58386c3c33e81afea20581e3d11e9137fb04535a
Thais Cardoso <thaistauane.cardoso@gmail.com>
2022-05-22 18:50:35 -0300

    feat/ fix: req 2 - route /users/id - corrigindo erros no req 1,

--- routes/talker.js
+++ routes/talker.js
@@ -4,20 +4,32 @@ const fs = require('fs');
 
 const routes = express.Router();
 const talkerData = 'talker.json';
+const readFile = fs.readFileSync(talkerData);
+const talker = JSON.parse(readFile);
 
 routes.get('/', (_req, res) => {
-    const readFile = fs.readFileSync(talkerData);
-    const talker = JSON.parse(readFile);
     res.status(200).json(talker);
 });
 routes.get('/search', (req, res) => {
     const { name } = req.query;
-    const readFile = fs.readFileSync(talkerData);
-    const talker = JSON.parse(readFile);
 
     if (name === '' || undefined) {
         return res.status(200).json(talker);
     }
+
+    const talkerperson = talker.filter((el) =>
+     el.name.toLowerCase().includes(name.toLocaleLowerCase()));
+
+    res.status(200).json(talkerperson);
+});
+
+routes.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const talkerId = talker.find((el) => el.id === Number(id));
+
+    if(!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+    res.status(200).json(talkerId);
 });
 
 module.exports = routes;
