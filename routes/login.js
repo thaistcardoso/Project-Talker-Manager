8af601e04dd714815927b06b61edfcc910a0eb58
Thais Cardoso <thaistauane.cardoso@gmail.com>
2022-05-22 22:24:29 -0300

    corrigindo constantes em lugar errado, gerrando erro na API

---
+++ routes/login.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const routes = express.Router();
+// const talkerData = 'talker.json';
+
+// routes.post('/', (req, res) => {
+    // const { email, password } = req.body;
+    
+// });
+
+module.exports = routes;
--- routes/talker.js
+++ routes/talker.js
@@ -4,13 +4,15 @@ const fs = require('fs');
 
 const routes = express.Router();
 const talkerData = 'talker.json';
-const readFile = fs.readFileSync(talkerData);
-const talker = JSON.parse(readFile);
 
 routes.get('/', (_req, res) => {
+    const readFile = fs.readFileSync(talkerData);
+    const talker = JSON.parse(readFile);
     res.status(200).json(talker);
 });
 routes.get('/search', (req, res) => {
+    const readFile = fs.readFileSync(talkerData);
+    const talker = JSON.parse(readFile);
     const { name } = req.query;
 
     if (name === '' || undefined) {
@@ -18,16 +20,18 @@ routes.get('/search', (req, res) => {
     }
 
     const talkerperson = talker.filter((user) =>
-     user.name.toLowerCase().includes(name.toLocaleLowerCase()));
+        user.name.toLowerCase().includes(name.toLocaleLowerCase()));
 
     res.status(200).json(talkerperson);
 });
 
 routes.get('/:id', (req, res) => {
     const { id } = req.params;
+    const readFile = fs.readFileSync(talkerData);
+    const talker = JSON.parse(readFile);
     const talkerId = talker.find((user) => user.id === Number(id));
 
-    if (!talkerId) return res.status(404).json({ message: 'User not Found' });
+    if (!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
     res.status(200).json(talkerId);
 });
